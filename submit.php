00617990199873c5f1d5b933e574732f2c1068ec
unknown <gokulvijayanand@gmail.com>
2023-08-19 18:55:21 +0530

    updated contact page

---
+++ submit.php
@@ -0,0 +1,30 @@
+<?php
+// Establish a connection to the MySQL database
+$servername = "localhost";
+$username = "htmluser";
+$password = "html123";
+$dbname = "htmldata";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+// Insert data into the database
+$sql = "INSERT INTO contact_messages (name, email, subject, message) VALUES ('$name', '$email', '$subject', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Message sent successfully!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
